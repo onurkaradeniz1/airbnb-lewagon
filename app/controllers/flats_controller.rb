b4d8dd921829aab3e0b16a89dc3d0dde1450f9f5
Paula Garcia Llopis <paulagllopis@gmail.com>
2022-01-29 16:59:43 +0100

    flat model/controller

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,53 @@
+class FlatsController < ApplicationController
+  before_action :find_user
+  before_action :find_flat
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user_id = @user.id
+    @flat.save
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:price_per_day, :address, :pictures, :availability, :description, :capacity)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def find_user
+    @user = current_user.id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_150403) do
+ActiveRecord::Schema.define(version: 2022_01_29_152525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.integer "price_per_day"
+    t.string "address"
+    t.string "pictures"
+    t.date "availability"
+    t.text "description"
+    t.integer "capacity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_01_29_150403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
 end
