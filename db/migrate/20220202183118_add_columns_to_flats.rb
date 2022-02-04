391b75a2ff7eef405476768024f26552dae5f05f
Onur Karadeniz <onurkaradeniz1@gmail.com>
2022-02-04 10:09:33 +0100

    pundit is completed for both booking and flat model. Though I could not test cause I could not log out :)

--- db/migrate/20220202183118_add_columns_to_flats.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToFlats < ActiveRecord::Migration[6.1]
-  def change
-    add_column :bookings, :first_day_of_flat, :date
-    add_column :bookings, :last_day_of_flat, :date
-
-    remove_column :bookings, :date_booked
-  end
-end
