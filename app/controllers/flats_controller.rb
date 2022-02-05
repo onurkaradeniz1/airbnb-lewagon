ec1d0dab0ec77fa635e3e3e0d3ecfe9cf6353a93
Onur Karadeniz <onurkaradeniz1@gmail.com>
2022-02-05 16:03:29 +0100

    geocoding created but shitty design

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -5,6 +5,15 @@ class FlatsController < ApplicationController
   def index
     # @flats = Flat.all
     @flats = policy_scope(Flat)
+
+      @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { flat: flat }),
+        #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+      }
+    end
   end
 
   def new
