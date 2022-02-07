187430a25ba08c4700db73148c1584e6e9851ce3
George Gowers <georgegowers@gmail.com>
2022-02-07 18:09:59 +0100

    created seed file with Flats & Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "seeding the database"
+puts "cleaning up the database"
 
-user1 = User.create(
-  email: "george123@gmail.com",
-  password: 123321
-)
+Flat.destroy_all
+User.destroy_all
 
-puts "created user"
+puts "creating users"
 
+users = []
 
-100.times do
-  number = 1
+10.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: Faker::PhoneNumber.subscriber_number(length: 6)
+  )
+  users << user
+  puts "created user with id: #{user.id}"
+end
+
+puts "creating flats"
+
+50.times do
   flat = Flat.new(
     price_per_day: rand(50..300),
     address: Faker::Address.city,
-    start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
-    end_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+    start_date: Faker::Date.between(from: '2022-01-01', to: '2022-01-20'),
+    end_date: Faker::Date.between(from: '2014-01-21', to: '2014-02-10'),
     capacity: rand(2..6)
-  )
-  flat.user = user1
+    )
+  flat.user = users.sample
   flat.save
-  puts "created flat #{number}"
-  number += 1
+  puts "created flat with id: #{flat.id}"
 end
 
-puts "Completed!"
+puts "Done seeding, have fun coding!"
