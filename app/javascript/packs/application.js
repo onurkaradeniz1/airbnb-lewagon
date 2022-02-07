02bba59720dfd21d3d2d419819f31aee9808a44c
Marianna Safuler <msafuler@gmail.com>
2022-02-08 00:07:30 +0100

    booking part1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,19 +38,23 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   $('.dropdown-toggle').dropdown()
-  let start = moment().subtract(29, 'days');
-  let end = moment();
-
   function cb(start, end) {
-    //$('#reportrange span').html(start.format('D MMMM, YYYY') + ' - ' + end.format('D MMMM, YYYY'));
+    $('#reportrange span').html(start.format('D MMMM, YYYY') + ' - ' + end.format('D MMMM, YYYY'));
   }
 
   let $input = $('#reportrange');
+  let calendarData = document.querySelector('#calendar-data').dataset;
+  let start = moment(calendarData.startDate, "MM DD YYYY");
+  let end = start.add(1, 'days');
 
   $input.daterangepicker({
     startDate: start,
     endDate: end,
-    alwaysShowCalendars: true
+    minDate: start,
+    maxDate: moment(calendarData.endDate, "MM DD YYYY"),
+    alwaysShowCalendars: true,
+    autoUpdateInput: true,
+    au
   }, cb);
 
   cb(start, end);
